825304264ab04ce50a03afd6db3120945d4a13e1
Peter Olchawa <piotrusolchawa@gmail.com>
2010-06-18 22:09:21 +0200

    small refactoring

--- lotto.hs
+++ lotto.hs
@@ -1,34 +1,50 @@
-data Cell = Cell (Integer,Integer,Integer) deriving Show
+module Lotto (Cell, Row, Generator, Task(..)) where
+
+import Monad
+import List ((\\), delete)
+
+type Generator a = [a]
+
+type Cell = (Int,Int,Int)
+type BlackCoords = (Int, Int)
 type Row = [Cell]
 
-get_colliding :: [Row] -> ([[[Cell]]],[[[Cell]]])
-get_colliding table = (horizontally table, vertically table) where
-    horizontally :: [Row] -> [[[Cell]]]
-    horizontally matrix = map (process_row []) matrix
+data Task = Task {
+        size  :: Int,
+        board :: [[Int]]
+     }
+--Returns colliding values on the given board
+getColliding :: [Row] -> ([[[Cell]]],[[[Cell]]])
+getColliding table = (horizontally table, vertically table) where
+    horizontally matrix = filter (not.null) $ map (processRow []) matrix
     vertically matrix = horizontally $ transpose matrix 
-
-process_row result [] = result
-process_row result row = 
-    let choice = same (head row) (tail row)
-        row_rest = rest (head row) (tail row) in
-    if null choice then process_row result row_rest
-    else process_row (((head row):choice):result) row_rest 
-
-same (Cell (val,_,_)) row = filter collide row where
-    collide (Cell (val1,_,_)) = val == val1
-rest (Cell (val,_,_)) row = filter dontCollide row where
-    dontCollide (Cell (val1,_,_)) = val /= val1
 
 transpose matrix = map reverse $ foldl
     (flip $ zipWith (:))
     (replicate (length $ head matrix) [])
     matrix
+--Returns colliding values in the given row
+processRow result [] = result
+processRow result row = 
+    let options = same (head row) (tail row)
+        row_rest = rest (head row) (tail row) in
+    if null options then processRow result row_rest
+    else processRow (((head row):options):result) row_rest 
 
-create_matrix [] _ result = result
-create_matrix (row:rows) n result = 
-    create_matrix rows (n+1) ((reverse(create_row row n 0 [])):result)
-create_row [] _ _ result = result
-create_row (val:vals) n m result = create_row vals n (m+1) ((Cell (val,n,m)):result)
+same ((val,_,_)) row = [ cell | cell@(val1,_,_) <- row, val1 == val ]
+rest ((val,_,_)) row = [ cell | cell@(val1,_,_) <- row, val1 /= val ]
+--Creates matrix of cells from given table of values
+createMatrix [] _ result = result
+createMatrix (row:rows) n result = 
+    createMatrix rows (n+1) ((reverse(createRow row n 0 [])):result)
+createRow [] _ _ result = result
+createRow (val:vals) n m result = createRow vals n (m+1) (((val,n,m)):result)
 
 table = [[1,1],[2,1]]
-check = get_colliding (reverse $ create_matrix table 0 [])
+check = 
+    getColliding (reverse $ createMatrix table 0 [])
+--------------------------------------------------------------------
+solve :: Task -> Generator BlackCoords
+solve task = 
+    let x = size task in
+    [(1,2)]
